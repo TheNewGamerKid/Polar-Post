98daa5ec40c3ed5f343716f2da669c8f1c887ba1
TheNewGamerKid <robert.ikerd@outlook.com>
2023-12-20 11:29:00 -0800

    Getting Svelte and Tailwind set up

---
+++ src/components/RouteBar.svelte
@@ -0,0 +1,12 @@
+<section class="h-full flex flex-col mx-2">
+	<div class="grow">
+		<a href="/" class="text-sm w-20 inline-flex justify-center items-center h-full">Home</a>
+		<a href="/tutorials" class="text-sm w-20 inline-flex justify-center items-center h-full">
+			Tutorials
+		</a>
+		<a href="/blogs" class="text-sm w-20 inline-flex justify-center items-center h-full"> Blogs </a>
+		<a href="/discussions" class="text-sm w-20 inline-flex justify-center items-center h-full">
+			Discussions
+		</a>
+	</div>
+</section>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import RouteBar from '../components/RouteBar.svelte';
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;500;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main class="w-screen h-screen bg-slate-900">
+	<header class="w-screen h-16 bg-slate-800 text-slate-200 flex items-center px-3">
+		<a href="/" rel="nofollow" class="text-2xl mx-3">Polar Post</a>
+		<RouteBar />
+	</header>
+	<slot />
+</main>
